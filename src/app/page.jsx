448dabfb6e62acf37a6e424211aff40b4a8545de
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-13 17:50:21 +0200

    center the buttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Header appMode="solver" title="Fill in your challenge"/>
-      <div className="space-x-8">
+      <div className="space-x-8 flex justify-center">
         <Button variant="filled">Solve</Button>
         <Button variant="outlined">Clear</Button>
       </div>
