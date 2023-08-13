9de2e7ffafd7e7e8c98581877af7faba4b9b3344
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-13 17:43:45 +0200

    rebase with upstream

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
 import { Header } from "./components/Header"
+import { Button } from "./components/Button"
 
 export default function Home() {
   return (
     <main>
       <Header appMode="solver" title="Fill in your challenge"/>
+      <div className="space-x-8">
+        <Button variant="filled">Solve</Button>
+        <Button variant="outlined">Clear</Button>
+      </div>
     </main>
   )
 }
