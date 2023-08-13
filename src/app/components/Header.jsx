78942079d68dfec5968c167546e2ce18fd51c962
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-13 15:07:10 +0200

    include heading in Header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,18 @@
-export function Header({ appMode }) {
+export function Header({ title, appMode }) {
   return (
-    <div className="flex justify-between uppercase">
-      <div className="flex flex-col text-xs">
-        <span>Sudoku</span>
-        <span>Cruncher</span> 
+    <header>
+      <div className="flex justify-between uppercase">
+        <div className="flex flex-col text-xs">
+          <span>Sudoku</span>
+          <span>Cruncher</span> 
+        </div>
+        <div className="flex items-center text-sm">
+          <div className={`p-2 border-r-2 border-black ${appMode === "creator" && "text-gray-400"}`}>Solver</div> 
+          <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>Creator</div> 
+        </div>
       </div>
-      <div className="flex items-center text-sm">
-        <div className={`p-2 border-r-2 border-black ${appMode === "creator" && "text-gray-400"}`}>Solver</div> 
-        <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>Creator</div> 
-      </div>
-    </div>
+
+      <h1 className="text-4xl mt-6">{title}</h1>
+    </header>
   )
 }
