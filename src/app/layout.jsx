5957e0dd5e2d4890c67142dfa4624bae76e1c144
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-13 15:25:46 +0200

    render Header as part of page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Header } from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header appMode="solver" title="Fill in your challenge"/>
         {children}
       </body>
     </html>
