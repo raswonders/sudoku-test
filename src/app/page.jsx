c00a3ac10e5511bc217b8e50ffbc680d5a9e0c83
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-14 12:27:11 +0200

    add board to page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
 import { Button } from "./components/Button"
+import Board from "./components/board"
 
 export default function Home() {
   return (
     <main>
       <Header appMode="solver" title="Fill in your challenge"/>
+      <Board />
       <div className="space-x-8 flex justify-center mt-8">
         <Button variant="filled">Solve</Button>
         <Button variant="outlined">Clear</Button>
