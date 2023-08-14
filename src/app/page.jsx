1e013ae60c530fb4c2ade8635dc249990d61718d
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-15 00:58:05 +0200

    clear all cells after clicking Clear

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,10 @@ import Board from "./components/board";
 export default function Home() {
   const [cells, setCells] = useState(Array(81).fill(""));
 
+  function handleClearAll() {
+    setCells(Array(81).fill(''));
+  }
+
   return (
     <main className="px-4 py-6 w-full h-full flex justify-center">
       <div className="w-full h-full max-w-screen-lg flex flex-col justify-between items-center">
@@ -16,7 +20,7 @@ export default function Home() {
           <Board cells={cells} setCells={setCells} />
           <div className="space-x-8 mt-6 flex justify-center">
             <Button variant="filled">Solve</Button>
-            <Button variant="outlined">Clear</Button>
+            <Button variant="outlined" onClick={handleClearAll}>Clear</Button>
           </div>
         </div>
       </div>
