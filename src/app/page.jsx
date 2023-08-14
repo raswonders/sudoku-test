593dd61f45fca72fbe4adabc49e62e836f55a9c3
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-14 13:11:50 +0200

    restrict max width of content to 1024px

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,13 +4,15 @@ import Board from "./components/board";
 
 export default function Home() {
   return (
-    <main className="px-4 py-6 w-full h-full flex flex-col justify-between items-center">
-      <Header appMode="solver" title="Fill in your challenge" />
-      <div>
-        <Board />
-        <div className="space-x-8 mt-6 flex justify-center">
-          <Button variant="filled">Solve</Button>
-          <Button variant="outlined">Clear</Button>
+    <main className="px-4 py-6 w-full h-full flex justify-center">
+      <div className="w-full h-full max-w-screen-lg flex flex-col justify-between items-center">
+        <Header appMode="solver" title="Fill in your challenge" />
+        <div>
+          <Board />
+          <div className="space-x-8 mt-6 flex justify-center">
+            <Button variant="filled">Solve</Button>
+            <Button variant="outlined">Clear</Button>
+          </div>
         </div>
       </div>
     </main>
