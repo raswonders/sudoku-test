06c4075f55edfff39df7003fc1d26737c4c8bb70
didibu26 <88148058+didibu26@users.noreply.github.com>
2023-08-14 15:25:34 +0200

    added input check functionality in cell component

--- src/app/components/cell.jsx
+++ src/app/components/cell.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react"
 
-const regex= new RegExp("^[1-9]+$")
+import { useEffect, useState } from "react"
 
-export default function Cell( { name } ) {
+const INP_REGEX = /^(?:[1-9]|)$/;
 
-    const [number, setNumber] = useState("");
+export default function Cell( { row, col} ) {
 
-    var condition = "";
+    const [number, setNumber] = useState('');
+    const [validInput, setValidInput] = useState(true);
+
+    useEffect(() => {
+        setValidInput(INP_REGEX.test(number));
+    }, [number])
 
     const handleChange = (e) => {
         e.preventDefault();
@@ -16,6 +20,6 @@ export default function Cell( { name } ) {
     }
 
     return (
-        <input type="text" pattern="^[1-9]+$" value={number} className="w-10 h-10 border border-1 border-slate-400 text-center " onChange={(e) => handleChange(e)}></input>
+        <input type="text" value={number} className={`w-10 h-10 border border-1 border-slate-400 text-center ${validInput ? "" : "bg-red-400"}`} onChange={(e) => handleChange(e)}></input>
     )
 }
