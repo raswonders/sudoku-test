c3d5d02b44e40b44bace0fa64a527381d04326f9
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-14 23:07:12 +0200

    add responsive board

--- src/app/components/board.jsx
+++ src/app/components/board.jsx
@@ -1,34 +1,10 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import Cell from './cell'
-
-const board = [...Array(9)]
+import { useState } from "react";
+import Cell from "./cell";
 
 export default function Board() {
-  
-  const [size, setSize] = useState(9)
+  const cells = Array.from({ length: 81 }, (_, index) => <Cell key={index} />);
 
-  return (
-    <div className="flex flex-row justify-center items-center">
-      { 
-        board.map( (rKey, cIndex) => {
-          return (
-            <div key={cIndex}>
-                {
-                  board.map( (cKey, rIndex) => {
-                    return (
-                      <div className="" key={rIndex + cIndex}>
-                        <Cell key={rIndex} row={rIndex} col={cIndex}/>
-                      </div>
-                    ) 
-                  })
-                }
-            </div>
-            )
-          })
-        
-      }
-    </div>
-  )
+  return <div className="board">{cells}</div>;
 }
