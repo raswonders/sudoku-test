ee1e5291ca381e6818e7ee8987e8a693b55d8646
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-15 22:35:35 +0200

    allow only single digit input in board

--- src/app/components/cell.jsx
+++ src/app/components/cell.jsx
@@ -1,6 +1,19 @@
-import { useEffect, useState } from "react";
-
 export default function Cell({ value, isValid, handleChange }) {
+  function handleKeyDown(e) {
+    const bkspPressed = e.key === "Backspace" || e.keyCode === 8;
+    const deletePressed = e.key === "Delete" || e.keyCode === 46;
+    const digit = parseInt(e.key, 10);
+    if (!isNaN(digit) && digit >= 1 && digit <= 9) {
+      e.preventDefault();
+      handleChange(digit);
+    }
+
+    if (bkspPressed || deletePressed) {
+      e.preventDefault();
+      handleChange("");
+    }
+  }
+
   return (
     <input
       type="number"
@@ -9,6 +22,7 @@ export default function Cell({ value, isValid, handleChange }) {
         isValid ? "" : "bg-red-400"
       }`}
       onChange={(e) => handleChange(e.target.value)}
+      onKeyDown={(e) => handleKeyDown(e)}
     ></input>
   );
 }
