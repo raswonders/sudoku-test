741e70a79c0c444008bb186f37db48be0a69419e
didibu26 <88148058+didibu26@users.noreply.github.com>
2023-08-15 16:30:52 +0200

    built solve() function, reworked checkDuplicates

--- src/app/components/board.jsx
+++ src/app/components/board.jsx
@@ -2,11 +2,17 @@
 
 import Cell from "./cell";
 
-export default function Board({ cells, setCells }) {
+export default function Board({ cells, setCells, checkDuplicates }) {
+    
   function handleInputChange(index, value) {
     const updatedCells = [...cells];
     updatedCells[index] = value;
+    
+    if(!checkDuplicates(cells, index)){
+      updatedCells[index] = '';
+    } else {
     setCells(updatedCells);
+    }
   }
 
   return (
@@ -22,45 +28,3 @@ export default function Board({ cells, setCells }) {
   );
 }
 
-function checkDuplicates(cells, index){
-  indicesToCheck = Array(20).fill(null);
-  /* I need to check the row, the column and the remaining 4 cells of the 3x3 grid*/
-
-  /* first for-loop for the row*/
-  for(let r = index % 9; r < index; r++){ // Left side
-    indicesToCheck.push(r)
-  }
-  for(let r = index + 1; r < index % 9 + 9; r++){ // Right side
-    indicesToCheck.push(r)
-  }
-
-   /* then for-loop for the column*/
-  for (let r = index; r < 81; r+=9){
-    indicesToCheck.push(r)
-  }
-  for (let r = index % 9; r < index; r+=9){
-    indicesToCheck.push(r)
-  }
-
-  // Find the top left index, for a given 3x3 square
-  var pos = index;
-  pos = index - index%3;
-  our_row = Math.floor(pos / 9)
-  pos = pos - 9 * (our_row % 3)
-
-  /* then for-loop for the remaining 4 cells of the 3x3 grid*/
-  for (let r = pos; r < pos + 2; r++){
-    for (let v = r; v < r + 19; v+=9){
-        indicesToCheck.push(v)
-    }
-  }
-
-  for (var i of indicesToCheck){
-    if (i != index){
-      if (cells[i] == cells[index]){
-        // there's a duplicate!
-        return True;
-      }
-    }
-  }
-}
