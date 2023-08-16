1b3819cd15cbe0e4e3d1c0766e45a05c3a82a3a0
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-16 02:06:29 +0200

    render UI Buttons and Header conditionally

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,29 +6,34 @@ import { Button } from "./components/Button";
 import Board from "./components/board";
 
 export default function Home() {
-  const [cells, setCells] = useState(Array(81).fill({ value: "", isValid: true }));
+  const [cells, setCells] = useState(
+    Array(81).fill({ value: "", isValid: true })
+  );
+
+  const allValuesSet = cells.every((cell) => cell.value !== "");
+  const isValidBoard = cells.every((cell) => cell.isValid);
+  const isSolution = allValuesSet && isValidBoard;
 
   function handleClearAll() {
     setCells(Array(81).fill({ value: "", isValid: true }));
   }
 
-  function solve (cells) {
-    
+  function solve(cells) {
     const newCells = [...cells];
 
-    for(let i = 0; i < 81; i++){
-      if(newCells[i].value === ''){
-        for(let guess = 1; guess < 10; guess++){
-          newCells[i] = { ...newCells[i], value: guess.toString()};
-          if(!hasDuplicates(newCells, i)){
-            if(solve(newCells)){
+    for (let i = 0; i < 81; i++) {
+      if (newCells[i].value === "") {
+        for (let guess = 1; guess < 10; guess++) {
+          newCells[i] = { ...newCells[i], value: guess.toString() };
+          if (!hasDuplicates(newCells, i)) {
+            if (solve(newCells)) {
               return true;
             }
           } else {
-            newCells[i] = { ...newCells[i], value: ""};
+            newCells[i] = { ...newCells[i], value: "" };
           }
         }
-        return false
+        return false;
       }
     }
     setCells(newCells);
@@ -41,21 +46,21 @@ export default function Home() {
     const col = index % 9;
 
     if (value === "") return false;
-  
+
     // Check duplicates in the row
     for (let i = 0; i < 9; i++) {
       if (i !== col && cells[row * 9 + i].value === value) {
         return true;
       }
     }
-  
+
     // Check duplicates in the column
     for (let i = 0; i < 9; i++) {
       if (i !== row && cells[i * 9 + col].value === value) {
         return true;
       }
     }
-  
+
     // Check duplicates in the 3x3 box
     const startRow = Math.floor(row / 3) * 3;
     const startCol = Math.floor(col / 3) * 3;
@@ -66,21 +71,54 @@ export default function Home() {
         }
       }
     }
-  
+
     return false;
-  }    
-  
+  }
 
   return (
     <main className="px-4 py-6 w-full h-full flex justify-center">
       <div className="w-full h-full max-w-screen-lg flex flex-col justify-between items-center">
-        <Header appMode="solver" title="Fill in your challenge" />
+        {!isSolution && (
+          <Header appMode="solver" title="Fill in your challenge" />
+        )}
+        {isSolution && <Header appMode="solver" title="Solution found!" />}
+
         <div className="flex-grow flex flex-col justify-center">
-          <Board cells={cells} setCells={setCells} hasDuplicates={hasDuplicates}/>
-          <div className="space-x-8 mt-10 flex justify-center">
-            <Button variant="filled" onClick={() => solve(cells)}>Solve</Button>
-            <Button variant="outlined" onClick={handleClearAll}>Clear</Button>
-          </div>
+          <Board
+            cells={cells}
+            setCells={setCells}
+            hasDuplicates={hasDuplicates}
+          />
+
+          {!isSolution && (
+            <div className="space-x-8 mt-10 flex justify-center">
+              <Button
+                variant="filled"
+                onClick={() => solve(cells)}
+                disabled={!isValidBoard}
+              >
+                Solve
+              </Button>
+              <Button variant="outlined" onClick={handleClearAll}>
+                Clear
+              </Button>
+            </div>
+          )}
+
+          {isSolution && (
+            <div className="space-x-8 mt-10 flex justify-center">
+              <Button
+                variant="filled"
+                onClick={() => alert("This feature is coming soon")}
+                disabled={!isValidBoard}
+              >
+                Export
+              </Button>
+              <Button variant="outlined" onClick={handleClearAll}>
+                Back
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </main>
