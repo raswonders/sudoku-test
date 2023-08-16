836b25ffb18fd917e2f45fa4ffae5fc99b41ae2b
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-16 02:12:12 +0200

    add little bit of ux for missing features

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,15 +4,26 @@ export function Header({ title, appMode }) {
       <div className="flex justify-between uppercase">
         <div className="flex flex-col text-xs">
           <span>Sudoku</span>
-          <span>Cruncher</span> 
+          <span>Cruncher</span>
         </div>
-        <div className="flex items-center text-sm">
-          <div className={`p-2 border-r-2 border-black ${appMode === "creator" && "text-gray-400"}`}>Solver</div> 
-          <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>Creator</div> 
+        <div
+          className="flex items-center text-sm select-none cursor-pointer"
+          onClick={() => alert("This feature is coming soon")}
+        >
+          <div
+            className={`p-2 border-r-2 border-black ${
+              appMode === "creator" && "text-gray-400"
+            }`}
+          >
+            Solver
+          </div>
+          <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>
+            Creator
+          </div>
         </div>
       </div>
 
       <h1 className="text-4xl mt-6 lg:text-center">{title}</h1>
     </header>
-  )
+  );
 }
