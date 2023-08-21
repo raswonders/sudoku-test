bbd722d77fe38bb9fd34d4dbcd4b50205b4115a9
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-21 15:03:02 +0200

    style focused cell

--- src/app/components/cell.jsx
+++ src/app/components/cell.jsx
@@ -1,6 +1,9 @@
 export default function Cell({ value }) {
   return (
-    <div className="flex justify-center items-center bg-white">
+    <div
+      className="flex justify-center items-center bg-white ring-inset focus:outline-none focus:bg-blue-100"
+      tabIndex={0}
+    >
       {value > 0 ? value : ""}
     </div>
   );
