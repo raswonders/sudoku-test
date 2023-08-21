7c11cfb71684acbc41203e524a83a98053afc013
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-21 13:23:46 +0200

    add responsive font for board

--- src/app/components/board.jsx
+++ src/app/components/board.jsx
@@ -1,20 +1,28 @@
 "use client";
 
+import { Quicksand } from "next/font/google";
 import Cell from "./cell";
 import { getCellsInGrid } from "../utils";
 
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weights: [400, 600],
+});
+
 export function Grid9x9({ cellValues, setCellValues }) {
   return (
-    <div className="grid9x9">
-      {Array(9).fill(null).map((_, i) => (
-        <Grid3x3 key={i} cells={getCellsInGrid(cellValues, i)} />
-      ))}
+    <div className={`grid9x9 ${quicksand.className} font-scaling`}>
+      {Array(9)
+        .fill(null)
+        .map((_, i) => (
+          <Grid3x3 key={i} cells={getCellsInGrid(cellValues, i)} />
+        ))}
     </div>
   );
 }
 
 export function Grid3x3({ cells }) {
-  console.log(cells)
+  console.log(cells);
   return (
     <div className="grid3x3 rounded-lg overflow-hidden umbra-6dp">
       {cells.map((value, i) => (
