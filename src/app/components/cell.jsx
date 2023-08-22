984fc3e7e7ea913bbf0129913ff8e4865f1bf9ca
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-08-22 12:49:40 +0200

    add error styling for cells

--- src/app/components/cell.jsx
+++ src/app/components/cell.jsx
@@ -6,16 +6,20 @@ export default function Cell({
   setFocusedCell,
   handleKeyDown,
   cellProtection,
+  cellErrors,
 }) {
   const hasFocusOnRowOrCol = row === focusedCell.row || col === focusedCell.col;
-  const isCellProtected = cellProtection[row][col];
+  const isProtected = cellProtection[row][col];
+  const hasError = cellErrors[row][col];
 
   return (
     <div
       className={`flex justify-center items-center ${
-        hasFocusOnRowOrCol ? "bg-blue-50" : "bg-white"
+        hasError ? "bg-red-100" : hasFocusOnRowOrCol ? "bg-blue-50" : "bg-white"
+      } ${
+        hasError && !isProtected ? "text-red-400" : ""
       } ring-inset focus:outline-none focus:bg-blue-100 select-none`}
-      tabIndex={isCellProtected ? undefined : 0}
+      tabIndex={isProtected ? undefined : 0}
       onFocus={() => setFocusedCell({ row, col })}
       onBlur={() => setFocusedCell({ row: null, col: null })}
       onKeyDown={(e) => handleKeyDown(e, row, col)}
