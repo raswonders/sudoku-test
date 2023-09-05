ddd0a71c05549db81aa53d6c08dfae7eee6f90a6
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-09-05 22:00:36 +0200

    style: change spinner to text-info color and make bg pure white

--- src/app/components/loading-spinner.jsx
+++ src/app/components/loading-spinner.jsx
@@ -2,8 +2,8 @@ function LoadingSpinner() {
   return (
     <>
       <dialog id="loading_spinner" className="modal">
-        <form method="dialog" className="modal-box w-auto bg-transparent">
-          <span className="loading loading-spinner loading-lg"></span>
+        <form method="dialog" className="modal-box w-auto">
+          <span className="loading loading-spinner loading-lg text-info"></span>
         </form>
       </dialog>
     </>
